360dc6e7be25932a89a7ecec8bf605b47f8f3da5
Ogrenci <Ogrenci@LAB5PC>
2025-02-05 10:45:05 +0300

    Add project files.

---
+++ merhabaGithub/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace merhabaGithub
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblgıthub = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // lblgıthub
+            // 
+            lblgıthub.AutoSize = true;
+            lblgıthub.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            lblgıthub.Location = new Point(43, 68);
+            lblgıthub.Name = "lblgıthub";
+            lblgıthub.Size = new Size(265, 45);
+            lblgıthub.TabIndex = 0;
+            lblgıthub.Text = "Merhaba Github";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.github;
+            pictureBox1.Location = new Point(411, 68);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(225, 225);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.d;
+            pictureBox2.Location = new Point(43, 116);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(147, 50);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblgıthub);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblgıthub;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
